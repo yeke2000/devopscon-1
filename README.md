c2fb7d144bfe6a492e9e73b3b76d985293df4c66
Nir Koren <nirk@liveperson.com>
2018-11-29 09:36:32 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to DevOpsCon demo repo
+## This repo contains basic Maven project with Hello-World WAR file 
+<BR> Ready for Tomcat deployment 
---
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -0,0 +1,10 @@
+package org.devopscon.maven.demo;
+
+public class HelloHandler {
+
+	public String sayHello() {
+		String hello = "Hello DevOpsCon members, What do you think about this conference?";
+		return hello;
+	}
+
+}
